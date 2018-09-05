5cfe2159cc32e750eaae6f5b69eb123116f4c867
Andre Hamman <hammanandre@gmail.com>
2018-09-05 16:08:25 +0200

    Basic outline for Ai structure
    Needs refactor
    Needs commenting and API Outline doc

---
+++ Assets/Scripts/AIResponse.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Response : UnityEvent<IResponse[]>{}
+public class AIResponse {
+	public AIResponse()
+    {
+		Response = new Response();
+		Response.AddListener(Manager._Instance.OnResponse);
+		ResponseChain = new List<IResponse>();
+    }
+	
+	public Response Response;
+
+	public List<IResponse> ResponseChain;
+	public void onTick(IData data)
+	{
+		cost++;
+	}
+	public int Cost{
+		get {
+			return cost;
+			}
+		}
+	private int cost;
+    public bool spawn(Spawnable spawnable,int lane)
+	{
+		IResponse response ;
+		/* fail the Spawn */
+		if(false)
+		{
+			return false;
+		}else{
+			return true;
+			ResponseChain.Add(response);
+		}
+	}
+	public bool finalizeResponse()
+	{
+		/* fail the finalize */
+		if(false)
+		{
+			return false;
+		}else{
+			Response.Invoke(ResponseChain.ToArray());
+			ResponseChain.Clear();
+			return true;
+		}
+	}
+	public IResponse[] QueryResponse()
+	{
+		return ResponseChain.ToArray();
+	}
+
+}
+public enum Spawnable
+{
+	Unicorn,
+	Bunny
+}
