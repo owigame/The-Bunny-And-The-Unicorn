766e5453c72bd64f39980d17b73f7a44762c87e5
Andre Hamman <hammanandre@gmail.com>
2018-09-05 20:11:25 +0200

    Included AI script Generators
    Look in create section for  AI script and it then auto adds the right template with super basic functionality, should help with on boarding

---
+++ Assets/Scripts/CodeTemplates/Editor/CodeTemplates.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+namespace XORHEAD.CodeTemplates
+{
+	public class DoCreateCodeFile : EndNameEditAction
+	{
+		public override void Action (int instanceId, string pathName, string resourceFile)
+		{
+			Object o = CodeTemplates.CreateScript(pathName, resourceFile);
+			ProjectWindowUtil.ShowCreatedAsset (o);
+		}
+	}
+
+	/// <summary>
+	/// Editor class for creating code files from templates.
+	/// </summary>
+	public class CodeTemplates 
+	{
+		/// <summary>
+		/// The C# script icon.
+		/// </summary>
+		private static Texture2D scriptIcon = (EditorGUIUtility.IconContent ("cs Script Icon").image as Texture2D);
+		
+		internal static UnityEngine.Object CreateScript(string pathName, string templatePath)
+		{
+			string newFilePath = Path.GetFullPath (pathName);
+			string fileNameWithoutExtension = Path.GetFileNameWithoutExtension (pathName);
+			string className = NormalizeClassName(fileNameWithoutExtension);
+
+			string templateText = string.Empty;
+
+			if (File.Exists(templatePath))
+			{
+				using (var sr = new StreamReader (templatePath))
+				{
+					templateText = sr.ReadToEnd();
+				}
+
+				templateText = templateText.Replace ("##NAME##", className);
+				
+				UTF8Encoding encoding = new UTF8Encoding (true, false);
+				
+				using (var sw = new StreamWriter (newFilePath, false, encoding))
+				{
+					sw.Write (templateText);
+				}
+				
+				AssetDatabase.ImportAsset (pathName);
+				
+				return AssetDatabase.LoadAssetAtPath (pathName, typeof(Object));
+			}
+			else
+			{
+				Debug.LogError(string.Format("The template file was not found: {0}", templatePath));
+
+				return null;
+			}
+		}
+
+		private static string NormalizeClassName(string fileName)
+		{
+			return fileName.Replace(" ", string.Empty);
+		}
+
+		/// <summary>
+		/// Creates a new code file from a template file.
+		/// </summary>
+		/// <param name="initialName">The initial name to give the file in the UI</param>
+		/// <param name="templatePath">The full path of the template file to use</param>
+		public static void CreateFromTemplate(string initialName, string templatePath)
+		{
+			ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
+				0,
+				ScriptableObject.CreateInstance<DoCreateCodeFile>(),
+				initialName,
+				scriptIcon,
+				templatePath);
+		}
+	}
+}
---
+++ Assets/Scripts/CodeTemplates/Editor/CodeTemplatesMenuItems.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace XORHEAD.CodeTemplates
+{
+	/// <summary>
+	/// Definitions for all the Code templates menu items.
+	/// </summary>
+	public class CodeTemplatesMenuItems
+	{
+		/// <summary>
+		/// The root path for code templates menu items.
+		/// </summary>
+		private const string MENU_ITEM_PATH = "Assets/Create/";
+
+		/// <summary>
+		/// Menu items priority (so they will be grouped/shown next to existing scripting menu items).
+		/// </summary>
+		private const int MENU_ITEM_PRIORITY = 75;
+			
+		/// <summary>
+		/// Creates a new C# class.
+		/// </summary>
+		[MenuItem(MENU_ITEM_PATH + "AI Script", false, MENU_ITEM_PRIORITY)]
+		private static void CreateClass()
+		{
+			CodeTemplates.CreateFromTemplate(
+				"NewAI.cs", 
+				@"Assets/scripts/CodeTemplates/Editor/Templates/AITemplate.txt");
+		}
+	}
+}
