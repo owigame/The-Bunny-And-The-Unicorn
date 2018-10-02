9367b38ec7aabe9ddf2f8df2d70f344d850d430e
Nicola <150128@virtualwindow.co.za>
2018-10-02 21:21:12 +0200

    Testing out some stuff.

---
+++ Assets/AI/Nicola/Test.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using AI;
+
+[CreateAssetMenu(fileName = "Test", menuName = "AI/Test", order = 0)]
+public class Test : LogicBase
+{
+    public override void OnTick(IBoardState data)
+    {
+        //if (!AIResponse.Spawn(Spawnable.Unicorn,1))
+        //      {}
+
+        //---creaturecount is always 3? --WHY?
+
+        //--have at least three creatures in play..spawn on lanes with enemies first... --HOW?
+        if (_Creatures.Count <= 3)
+        {
+            int randomLane = Random.Range(1, TournamentManager._instance.lanes.Count + 1);
+            //--attampt spawn in lane---
+            if (!AIResponse.Spawn(Random.Range(0, 2) == 0 ? Spawnable.Bunny : Spawnable.Unicorn, randomLane))
+            {
+                //--get a random creature to move/attack with... --HOW do I choose a type of creature?
+                CreatureBase randomCreature = _Creatures[Random.Range(0, _Creatures.Count)];
+                AttemptAttack(randomCreature);
+            }
+        }
+        else
+        {
+            //--get a random creature to move/attack with...
+            CreatureBase randomCreature = _Creatures[Random.Range(0, _Creatures.Count)];
+            AttemptAttack(randomCreature);
+        }
+        //IResponse[] responses = AIResponse.QueryResponse();
+        AIResponse.FinalizeResponse();
+    }
+
+    void AttemptAttack(CreatureBase creature)
+    {
+        if (creature != null)
+        {
+            List<CreatureBase> searchTargetCreatures = creature.ActiveLaneNode.laneManager.SearchRange((int)creature.Range, creature.ActiveLaneNode, this);
+            bool foundAttackTarget = false;
+            //---is target in attack range?
+            foreach (CreatureBase _creature in searchTargetCreatures)
+            {
+                if (_creature.Owner != creature.Owner)
+                {
+                    //Found enemy creature in range
+                    foundAttackTarget = true;
+                    AIResponse.Attack(creature);
+                }
+            }
+            if (!foundAttackTarget)
+            {
+                //Can't attack...
+                AttemptMove(creature);
+            }
+        }
+    }
+
+    void AttemptMove(CreatureBase creature)
+    {
+        int moveSpaces = creature.ActiveLaneNode.laneManager.GetOpenNodes(creature.ActiveLaneNode, creature.RightFacing);
+        if (moveSpaces > AIResponse.Tokens)
+        {
+            moveSpaces = AIResponse.Tokens;
+        }
+        AIResponse.Move(creature, moveSpaces);
+    }
+
+    void AttemptSpawn(int laneToSpawn)
+    {
+        AIResponse.Spawn(Random.Range(0, 2) == 0 ? Spawnable.Bunny : Spawnable.Unicorn, laneToSpawn);
+    }
+}
