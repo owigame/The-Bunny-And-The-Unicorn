fbd6beea7884c704b8b0975f0f8073112a873c6e
ChristopherFA1 <38582110+ChristopherFA1@users.noreply.github.com>
2018-10-03 12:57:52 +0200

    fixed stuff.

--- Assets/AI/Nicola/Test.cs
+++ Assets/AI/Nicola/Test.cs
@@ -1,35 +1,45 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using AI;
+using Logging;
 
 [CreateAssetMenu(fileName = "Test", menuName = "AI/Test", order = 0)]
 public class Test : LogicBase
 {
-    public override void OnTick(IBoardState data)
+    bool firstRun = true;
+    public override void OnTick(IBoardState[] data)
     {
+        if (firstRun)
+        {
+            _Creatures.Clear();
+            firstRun = false;
+        }
 
         //--have at least three creatures in play..spawn on lanes with enemies first... --HOW?
-        if (_Creatures.Count == 0)
-        {
-
-            int randomLane = Random.Range(1, TournamentManager._instance.lanes.Count + 1);
-            //--attampt spawn in lane---
-            if (!AIResponse.Spawn(Random.Range(0, 2) == 0 ? Spawnable.Bunny : Spawnable.Unicorn, randomLane))
+        
+       // if (_Creatures.Count == 0)
+      //  {
+            if (_Creatures.Count < 5)
             {
-                //--get a random creature to move/attack with... --HOW do I choose a type of creature?
+
+                int randomLane = Random.Range(1, TournamentManager._instance.lanes.Count + 1);
+                //--attampt spawn in lane---
+                if (!AIResponse.Spawn(Random.Range(0, 2) == 0 ? Spawnable.Bunny : Spawnable.Unicorn, randomLane))
+                 {
+                     //--get a random creature to move/attack with... --HOW do I choose a type of creature?
+                    CreatureBase randomCreature = _Creatures[Random.Range(0, _Creatures.Count)];
+                    AttemptAttack(randomCreature);
+                }   
+            }
+            else
+            {
                 CreatureBase randomCreature = _Creatures[Random.Range(0, _Creatures.Count)];
                 AttemptAttack(randomCreature);
             }
-        }
-        else if (_Creatures.Count > 0)
-        {
-            //--get a random creature to move/attack with...
-            CreatureBase randomCreature = _Creatures[Random.Range(0, _Creatures.Count)];
-            AttemptAttack(randomCreature);
-        }
 
 
         //IResponse[] responses = AIResponse.QueryResponse();
+        //AIResponse.responses.clear();
         AIResponse.FinalizeResponse();
     }
 
@@ -47,28 +57,27 @@ public class Test : LogicBase
                     //Found enemy creature in range
                     foundAttackTarget = true;
                     AIResponse.Attack(creature);
+                    AttemptMove(creature);
                 }
             }
             if (!foundAttackTarget)
             {
-                //Can't attack...
-                float wait = Random.Range(0, 25);
-                if (wait % 2 == 0)
-                {
-                    AttemptMove(creature);
-                }
+                AttemptMove(creature);
             }
         }
     }
 
     void AttemptMove(CreatureBase creature)
     {
-        int moveSpaces = creature.ActiveLaneNode.laneManager.GetOpenNodes(creature.ActiveLaneNode, creature.RightFacing);
-        if (moveSpaces > AIResponse.Tokens)
+        if(creature != null)
         {
-            moveSpaces = AIResponse.Tokens;
+            int moveSpaces = creature.ActiveLaneNode.laneManager.GetOpenNodes(creature.ActiveLaneNode, creature.RightFacing);
+            if (moveSpaces > AIResponse.Tokens)
+            {
+                moveSpaces = AIResponse.Tokens;
+            }
+            AIResponse.Move(creature, moveSpaces);
         }
-        AIResponse.Move(creature, moveSpaces);
     }
 
     void AttemptSpawn(int laneToSpawn)
