a52d01704f03d42cb32d6ae669bc3f5d15e2c11b
Nicola <150128@virtualwindow.co.za>
2018-10-06 14:01:25 +0200

    updated ai

--- Assets/AI/Nicola/Kittyv2.cs
+++ Assets/AI/Nicola/Kittyv2.cs
@@ -28,12 +28,23 @@ public class Kittyv2 : LogicBase
         if (enemyCount != 0)
         {
             FindNearest();
-            Debug.Log(targetCreature.name + " | " + targetCreature.ActiveLaneNode);
+        }
+
+        //--if neareast found, move towards and attack--
+        if (targetCreature != null)
+        {
+            AttemptAttackMoveTarget(targetCreature);
+        }
+        //--else random move or attack--
+        else
+        {
+            AttemptMoveAttack();
         }
 
         AIResponse.FinalizeResponse();
     }
 
+    //--Get total enemies in play--
     void GetCount()
     {
         //--reset count each round...
@@ -47,6 +58,7 @@ public class Kittyv2 : LogicBase
         }
     }
 
+    //--Find enemy nearest player--
     void FindNearest()
     {
         List<CreatureBase> closestCreatures = new List<CreatureBase>();
@@ -68,7 +80,7 @@ public class Kittyv2 : LogicBase
             //--add to list
             closestCreatures.Add(closestEnemy);
         }
-        Debug.Log("Closest Creature count: " + closestCreatures.Count);
+       // Debug.Log("Closest Creature count: " + closestCreatures.Count);
 
         //--Find the nearest from the list
         foreach (CreatureBase creature in closestCreatures)
@@ -82,6 +94,74 @@ public class Kittyv2 : LogicBase
                     highestLaneProgress = creature.LaneProgress;
                     targetCreature = creature;
                 }
+                else
+                {
+                    targetCreature = null;
+                }
+            }
+        }
+    }
+
+    //--Random attack or move--
+    void AttemptMoveAttack()
+    {
+        if (_Creatures.Count > 0)
+        {
+        Debug.Log("Attack or Move");
+            CreatureBase toMove = _Creatures[Random.Range(0, _Creatures.Count)];
+            // int distance = toMove.ActiveLaneNode.laneManager.GetOpenNodes(toMove.ActiveLaneNode, _RightFacing);
+            // AIResponse.Move(toMove, distance);
+
+            if (toMove != null)
+            {
+                List<CreatureBase> searchTargetCreatures = toMove.ActiveLaneNode.laneManager.SearchRange((int)toMove.Range, toMove.ActiveLaneNode, this);
+                bool foundAttackTarget = false;
+                foreach (CreatureBase _creature in searchTargetCreatures)
+                {
+                    if (_creature.Owner != toMove.Owner)
+                    { //Found enemy creature in range
+                        foundAttackTarget = true;
+                        AIResponse.Attack(toMove);
+                    }
+                }
+                if (!foundAttackTarget)
+                {
+                    int moveSpaces = toMove.ActiveLaneNode.laneManager.GetOpenNodes(toMove.ActiveLaneNode, _RightFacing);
+                    if (moveSpaces > AIResponse.Tokens)
+                    {
+                        moveSpaces = AIResponse.Tokens;
+                    }
+                    AIResponse.Move(toMove, moveSpaces);
+                }
+            }
+        }
+    }
+
+    void AttemptAttackMoveTarget(CreatureBase creature)
+    {
+        //Find friendly in same lane as _targetCreature
+        CreatureBase closestFriendly = creature.ActiveLaneNode.laneManager.GetFriendliesInLane(this)[0];
+        if (closestFriendly == null)
+        {
+            //No friendly, do something else...
+            if (!AIResponse.Spawn(Random.Range(0, 2) == 0 ? Spawnable.Bunny : Spawnable.Unicorn, creature.ActiveLaneNode.laneManager.LaneNumber))
+            {
+                //
+            }
+        }
+        else
+        {
+            //--Attempt to attack...
+            int openNodes = targetCreature.ActiveLaneNode.laneManager.GetOpenNodes(closestFriendly.ActiveLaneNode, _RightFacing);
+            //--is enemy in attack range? Attack...
+            if (openNodes < closestFriendly.Range)
+            {
+                AIResponse.Attack(closestFriendly);
+            }
+            //--Else move...
+            else
+            {
+                AIResponse.Move(closestFriendly, openNodes);
             }
         }
     }
